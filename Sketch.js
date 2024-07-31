e66cc3674374a1dd7a056d923c977e4b09657a0a
Fábio Toshio <00001125849630sp@al.educacao.sp.gov.br>
2024-07-31 14:53:28 -0300

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1000, 800);
+  background("rgb(38,38,190)");
+}
+
+function draw() {
+  
+  
+  fill("rgb(255,255,6)");
+  stroke("lightblue");
+  
+  if(mouseIsPressed);{
+  circle(mouseX,mouseY,40);
+  }
+}
